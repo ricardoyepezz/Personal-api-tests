dca077fc7d6d96cdbb25898b59d03aec516ff56f
ricardoyepezz <ricardoyepezz@gmail.com>
2022-03-21 17:57:06 +0000

    put method working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,9 +3,36 @@ import React, { useEffect, useState } from "react";
 const Home = () => {
 	const [datos, setDatos] = useState([]);
 	useEffect(() => {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify([
+			{
+				label: "hola1",
+				done: false,
+			},
+			{
+				label: "hola2",
+				done: false,
+			},
+		]);
+
+		var requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow",
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/ricardoyepez",
+			requestOptions
+		)
+			.then((response) => response.text())
+			.then((result) => console.log(result))
+			.catch((error) => console.log("error", error));
 		getTask();
 	}, []);
-
 	function getTask() {
 		var requestOptions = {
 			method: "GET",
